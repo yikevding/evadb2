3b8e47c1633fc526cb0ee561f3188944be319a06
Yi Ding <65103983+yikevding@users.noreply.github.com>
2023-11-21 15:15:02 +0800

    Finish request-2

--- request-2.py
+++ request-2.py
@@ -1,5 +1,5 @@
-def is_divisible_by_3_and_5(num):
-    if num % 3 == 0 and num % 5 == 0:
-        return True
-    else:
-        return False
+def is_strictly_decreasing(list_of_integers):
+    for i in range(len(list_of_integers) - 1):
+        if list_of_integers[i] <= list_of_integers[i+1]:
+            return False
+    return True
